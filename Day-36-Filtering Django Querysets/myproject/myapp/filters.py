82dc66fc27145bd6eea9f3f3ac68b10b14e456c2
vmopidi-endpoint <vmopidi@endpoint.com>
2024-04-23 11:00:01 +0530

    change

--- Day-36-Filtering Django Querysets/myproject/myapp/filters.py
+++ Day-36-Filtering Django Querysets/myproject/myapp/filters.py
@@ -1,8 +1,11 @@
 import django_filters
 from .models import Book
-
+from django import forms
 class BookFilter(django_filters.FilterSet):
     price=django_filters.RangeFilter()
+    genre=django_filters.MultipleChoiceFilter(Choices=Book.GenreChoices.choices,
+                                            widget=forms.CheckboxSelectMultiple()
+                                            )
     class Meta:
         model=Book
         #fields=['name','author__name','price','genre']
@@ -10,6 +13,6 @@ class BookFilter(django_filters.FilterSet):
         'name':['icontains'] ,
         'author__name':['icontains'],
         #'price':['lt','gt'],
-        'genre':['exact']
+        #'genre':['exact']
         }
 
